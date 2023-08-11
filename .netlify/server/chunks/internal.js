6826c9473a89a81cb8cc1a6d1ffac2b296b03766
Evan Marie Carr <evanmariecarr@gmail.com>
2023-08-11 08:45:27 -0400

    trying netlify

--- .netlify/server/chunks/internal.js
+++ .netlify/server/chunks/internal.js
@@ -193,7 +193,7 @@ const options = {
 		<div class="error">
 			<span class="status">` + status + '</span>\n			<div class="message">\n				<h1>' + message + "</h1>\n			</div>\n		</div>\n	</body>\n</html>\n"
   },
-  version_hash: "1a47cdx"
+  version_hash: "45dj75"
 };
 function get_hooks() {
   return {};
